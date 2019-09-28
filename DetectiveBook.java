1f61f5f40c22a70e632ca53f48ba2fbdda430062
Adil Saju <adilsaju@gmail.com>
2019-09-28 22:14:54 +0530

    completed

--- DetectiveBook.java
+++ DetectiveBook.java
@@ -12,10 +12,23 @@ public class DetectiveBook {
         for (int i = 0; i < n; i++) {
             ar.add(s.nextInt());
         }
-        for (int i = 0; i < ar.size(); i++) {
-            System.out.print(ar.get(i)+" ");
+        // for (int i = 0; i < n; i++) {
+        // System.out.print(ar.get(i)+" ");
+        // }
+        int mp = ar.get(0);
+        int noofdays = 0;
+        for (int i = 0; i < n; i++) {
+            if (ar.get(i) == i + 1 && ar.get(i) == mp) {
+                noofdays++;
+                mp++;
+                // continue;
+            } else if (ar.get(i) > i + 1 && ar.get(i) > mp) {
+                // update current mp
+                mp = ar.get(i);
+                // continue;
+            }
         }
-
+        System.out.println(noofdays);
         s.close();
     }
 
